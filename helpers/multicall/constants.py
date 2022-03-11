a403b42c7a994c34e191067e828823b212ee9c87
Ayush Shukla <shuklaayush247@gmail.com>
2022-03-11 18:34:53 +0530

    chore: add fantom multicall

--- helpers/multicall/constants.py
+++ helpers/multicall/constants.py
@@ -8,6 +8,7 @@ class Network(IntEnum):
     Rinkeby = 4
     Görli = 5
     xDai = 100
+    Fantom = 250
     Forknet = 1337
     BSC = 56
     Polygon = 137
@@ -21,6 +22,7 @@ MULTICALL_ADDRESSES = {
     Network.Rinkeby: "0x42Ad527de7d4e9d9d011aC45B31D8551f8Fe9821",
     Network.Görli: "0x77dCa2C955b15e9dE4dbBCf1246B4B85b651e50e",
     Network.xDai: "0xb5b692a88BDFc81ca69dcB1d924f59f0413A602a",
+    Network.Fantom: "0xb828C456600857abd4ed6C32FAcc607bD0464F4F",
     Network.Forknet: "0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441",
     Network.BSC: "0xec8c00da6ce45341fb8c31653b598ca0d8251804",
     Network.Polygon: "0x11ce4B23bD875D7F5C6a31084f55fDe1e9A87507",
