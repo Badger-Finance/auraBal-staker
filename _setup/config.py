1f0ff924c84b4e653f3cf92bf3452372b0a717dd
Ayush Shukla <shuklaayush247@gmail.com>
2022-06-28 13:19:51 +0400

    chore: only 10% performance fee

--- _setup/config.py
+++ _setup/config.py
@@ -3,11 +3,12 @@ WANT = "0x616e8BfA43F920657B3497DBf40D6b1A02D4608d"
 
 ## Account that has a lot of want (we will "borrow it" for testing)
 WHALE_ADDRESS = "0xfc78f8e1Af80A3bF5A1783BB59eD2d1b10f78cA9"
+
 ## Address for Badger Registry, used to fill in default addresses
 ## See: https://github.com/Badger-Finance/badger-registry
 REGISTRY = "0xFda7eB6f8b7a9e9fCFd348042ae675d1d652454f"
 
-PERFORMANCE_FEE_GOVERNANCE = 690  ## 6.9%
+PERFORMANCE_FEE_GOVERNANCE = 1000  ## 10%
 PERFORMANCE_FEE_STRATEGIST = 0  ## 0%
-WITHDRAWAL_FEE = 10  ## 0.1%
+WITHDRAWAL_FEE = 0  ## 0.1%
 MANAGEMENT_FEE = 0  ## 0%
