ec79ed71595540dd2ef1985dba25af8418502e5e
Ayush Shukla <shuklaayush247@gmail.com>
2022-07-29 02:46:45 +0400

    feat: add basic autocompounding code

--- contracts/AuraBalStakerStrategy.sol
+++ contracts/AuraBalStakerStrategy.sol
@@ -22,6 +22,7 @@ contract AuraBalStakerStrategy is BaseStrategy {
 
     bool public claimRewardsOnWithdrawAll;
     uint256 public balEthBptToAuraBalMinOutBps;
+    uint256 public minBbaUsdHarvest;
 
     IBaseRewardPool public constant AURABAL_REWARDS =
         IBaseRewardPool(0x5e5ea2048475854a5702F5B8468A51Ba1296EFcC);
@@ -47,11 +48,21 @@ contract AuraBalStakerStrategy is BaseStrategy {
         IERC20Upgradeable(0x5c6Ee304399DBdB9C8Ef030aB642B10820DB8F56);
     IERC20Upgradeable public constant BB_A_USD =
         IERC20Upgradeable(0x7B50775383d3D6f0215A8F290f2C9e2eEBBEceb2);
+    IERC20Upgradeable public constant BB_A_USDC =
+        IERC20Upgradeable(0x9210F1204b5a24742Eba12f710636D76240dF3d0);
+    IERC20Upgradeable public constant USDC =
+        IERC20Upgradeable(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
 
     bytes32 public constant BAL_ETH_POOL_ID =
         0x5c6ee304399dbdb9c8ef030ab642b10820db8f56000200000000000000000014;
     bytes32 public constant AURABAL_BALETH_BPT_POOL_ID =
         0x3dd0843a028c86e0b760b1a76929d1c5ef93a2dd000200000000000000000249;
+    bytes32 public constant BB_A_USD_POOL_ID =
+        0x7b50775383d3d6f0215a8f290f2c9e2eebbeceb20000000000000000000000fe;
+    bytes32 public constant BB_A_USDC_POOL_ID =
+        0x9210f1204b5a24742eba12f710636d76240df3d00000000000000000000000fc;
+    bytes32 public constant USDC_WETH_POOL_ID =
+        0x96646936b91d6b9d7d0c47c496afbf3d6ec7b6f8000200000000000000000019;
 
     /// @dev Initialize the Strategy with security settings as well as tokens
     /// @notice Proxies will set any non constant variable you declare as default value
@@ -65,6 +76,7 @@ contract AuraBalStakerStrategy is BaseStrategy {
 
         claimRewardsOnWithdrawAll = true;
         balEthBptToAuraBalMinOutBps = 9500; // max 5% slippage
+        minBbaUsdHarvest = 1000e18;
 
         AURABAL.safeApprove(address(AURABAL_REWARDS), type(uint256).max);
 
@@ -89,6 +101,12 @@ contract AuraBalStakerStrategy is BaseStrategy {
         balEthBptToAuraBalMinOutBps = _minOutBps;
     }
 
+    function seuMinBbaUsdHarvest(uint256 _minBbaUsd) external {
+        _onlyGovernanceOrStrategist();
+
+        minBbaUsdHarvest = _minBbaUsd;
+    }
+
     /// @dev Return the name of the strategy
     function getName() external pure override returns (string memory) {
         return "AuraBalStakerStrategy";
@@ -104,6 +122,7 @@ contract AuraBalStakerStrategy is BaseStrategy {
         override
         returns (address[] memory)
     {
+        // TODO: Check
         address[] memory protectedTokens = new address[](4);
         protectedTokens[0] = want; // AURABAL
         protectedTokens[1] = address(AURA);
@@ -154,22 +173,69 @@ contract AuraBalStakerStrategy is BaseStrategy {
         AURABAL_REWARDS.getReward();
 
         // Rewards are handled like this:
-        // BB_A_USD  --> B-BB_A_USD (emitted)
+        // BB_A_USD  --> AURABAL (autocompounded)
         // BAL       --> BAL/ETH BPT --> AURABAL (autocompounded)
         // AURA      --> GRAVIAURA (emitted)
-        harvested = new TokenAmount[](3);
-        harvested[0].token = address(B_BB_A_USD);
-        harvested[1].token = address(AURABAL);
-        harvested[2].token = address(GRAVIAURA);
+        harvested = new TokenAmount[](2);
+        harvested[0].token = address(AURABAL);
+        harvested[1].token = address(GRAVIAURA);
 
-        // BB_A_USD --> B_BB_A_USD
+        // BB_A_USD --> WETH
         uint256 bbaUsdBalance = BB_A_USD.balanceOf(address(this));
-        if (bbaUsdBalance > 0) {
-            B_BB_A_USD.deposit(bbaUsdBalance);
-            uint256 b_bbaUsdBalance = B_BB_A_USD.balanceOf(address(this));
+        uint256 wethEarned;
+        if (bbaUsdBalance > minBbaUsdHarvest) {
+            IAsset[] memory assets = new IAsset[](4);
+            assets[0] = IAsset(address(BB_A_USD));
+            assets[1] = IAsset(address(BB_A_USDC));
+            assets[2] = IAsset(address(USDC));
+            assets[3] = IAsset(address(WETH));
 
-            harvested[0].amount = b_bbaUsdBalance;
-            _processExtraToken(address(B_BB_A_USD), b_bbaUsdBalance);
+            IBalancerVault.BatchSwapStep[]
+                memory swaps = new IBalancerVault.BatchSwapStep[](3);
+
+            // BB_A_USD --> BB_A_USDC
+            swaps[0] = IBalancerVault.BatchSwapStep({
+                poolId: BB_A_USD_POOL_ID,
+                assetInIndex: 1, // TODO: No magic numbers
+                assetOutIndex: 3,
+                amount: bbaUsdBalance,
+                userData: new bytes(0)
+            });
+            // BB_A_USDC --> USDC
+            swaps[1] = IBalancerVault.BatchSwapStep({
+                poolId: BB_A_USDC_POOL_ID,
+                assetInIndex: 0,
+                assetOutIndex: 1,
+                amount: 0, // 0 means all from last step
+                userData: new bytes(0)
+            });
+            // USDC --> WETH
+            swaps[2] = IBalancerVault.BatchSwapStep({
+                poolId: USDC_WETH_POOL_ID,
+                assetInIndex: 0,
+                assetOutIndex: 1,
+                amount: 0, // 0 means all from last step
+                userData: new bytes(0)
+            });
+            int256[] memory limits = new int256[](3);
+
+            IBalancerVault.FundManagement memory fundManagement = IBalancerVault
+                .FundManagement({
+                    sender: address(this),
+                    fromInternalBalance: false,
+                    recipient: payable(address(this)),
+                    toInternalBalance: false
+                });
+
+            int256[] memory assetBalances = BALANCER_VAULT.batchSwap(
+                IBalancerVault.SwapKind.GIVEN_IN,
+                swaps,
+                assets,
+                fundManagement,
+                limits,
+                type(uint256).max
+            );
+            wethEarned = uint256(-assetBalances[assetBalances.length - 1]);
         }
 
         // BAL --> BAL/ETH BPT --> AURABAL
@@ -182,7 +248,7 @@ contract AuraBalStakerStrategy is BaseStrategy {
             assets[1] = IAsset(address(WETH));
             uint256[] memory maxAmountsIn = new uint256[](2);
             maxAmountsIn[0] = balBalance;
-            maxAmountsIn[1] = 0;
+            maxAmountsIn[1] = wethEarned;
 
             BALANCER_VAULT.joinPool(
                 BAL_ETH_POOL_ID,
